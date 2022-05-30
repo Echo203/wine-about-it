5a6989ec39b0093ba8da9fd94fce89b23b753bbd
Artur Kazuś <tksd97@wp.pl>
2022-05-30 18:18:06 +0200

    QtAbout Section setup

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -9,17 +9,15 @@ export default function Hero() {
   const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
 
   return (
-    <div className="hero">
+    <motion.div
+      exit={{ opacity: 0, translateX: "100px" }}
+      transition={transition}
+      className="hero"
+    >
       <div className="hero-container">
         <div className="hero-container-left container-half">
           {/* <WineBottle /> */}
-          <motion.h1
-            exit={{ opacity: 0 }}
-            transition={transition}
-            className="handwritten title"
-          >
-            Wine About It
-          </motion.h1>
+          <h1 className="handwritten title">Wine About It</h1>
           <p className="handwritten authors">by Maya & QtCinderella</p>
           <p className="hero-info-p">NEW EPISODE EVERY MONTH </p>
           <p className="hero-info-p">Watch live on:</p>
@@ -30,6 +28,6 @@ export default function Hero() {
           <p>Watch live on:</p> */}
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
---
+++ src/Components/QtAbout/QtAbout.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+import WineBottle from "../WineBottle/WineBottle";
+
+import "./Styles.scss";
+
+export default function QtAbout() {
+  const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
+
+  return (
+    <motion.div
+      exit={{ opacity: 0, translateX: "100px" }}
+      transition={transition}
+      className="QtAbout"
+    >
+      <div className="QtAbout-container">
+        <div className="QtAbout-container-left QtAbout-half"></div>
+        <div className="QtAbout-container-right QtAbout-half"></div>
+      </div>
+    </motion.div>
+  );
+}
