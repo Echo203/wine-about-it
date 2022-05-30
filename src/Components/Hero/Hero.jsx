13277c8e6933edc65832717acdd66d4defdbdeae
Artur Kazuś <tksd97@wp.pl>
2022-05-30 14:26:47 +0200

    Hero content and styling

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import WineBottle from "../WineBottle/WineBottle";
+
 import "./Styles.scss";
 
 export default function Hero() {
@@ -7,6 +9,7 @@ export default function Hero() {
     <div className="hero">
       <div className="hero-container">
         <div className="hero-container-left container-half">
+          <WineBottle />
           <h1 className="handwritten"> Wine About It</h1>
         </div>
         <div className="hero-container-right container-half">
