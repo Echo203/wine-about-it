23f2ab265623287bdde84c2be04e9005e5f39799
Artur Kazuś <tksd97@wp.pl>
2022-05-30 14:34:24 +0200

    Hero left content and styling

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -9,13 +9,16 @@ export default function Hero() {
     <div className="hero">
       <div className="hero-container">
         <div className="hero-container-left container-half">
-          <WineBottle />
+          {/* <WineBottle /> */}
           <h1 className="handwritten"> Wine About It</h1>
-        </div>
-        <div className="hero-container-right container-half">
           <h2 className="handwritten">by Maya & QtCinderella</h2>
           <p>NEW EPISODE EVERY MONTH </p>
           <p>Watch live on:</p>
+        </div>
+        <div className="hero-container-right container-half">
+          {/* <h2 className="handwritten">by Maya & QtCinderella</h2>
+          <p>NEW EPISODE EVERY MONTH </p>
+          <p>Watch live on:</p> */}
         </div>
       </div>
     </div>
