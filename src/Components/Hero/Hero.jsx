bd8e752ddad6238fd13838ef9f1ca54d1ccecb79
Artur Kazuś <tksd97@wp.pl>
2022-05-30 16:31:35 +0200

    Framer Motion and Animated Routes setup

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 import WineBottle from "../WineBottle/WineBottle";
 
 import "./Styles.scss";
 
 export default function Hero() {
+  const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
+
   return (
     <div className="hero">
       <div className="hero-container">
         <div className="hero-container-left container-half">
           {/* <WineBottle /> */}
-          <h1 className="handwritten title"> Wine About It</h1>
+          <motion.h1
+            exit={{ opacity: 0 }}
+            transition={transition}
+            className="handwritten title"
+          >
+            Wine About It
+          </motion.h1>
           <p className="handwritten authors">by Maya & QtCinderella</p>
           <p className="hero-info-p">NEW EPISODE EVERY MONTH </p>
           <p className="hero-info-p">Watch live on:</p>
