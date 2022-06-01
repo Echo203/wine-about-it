df519664c82cbfcfc9f70d602407b128d0328142
Artur Kazuś <tksd97@wp.pl>
2022-06-01 22:40:29 +0200

    Hero work

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,5 +6,12 @@ import "./Styles.scss";
 export default function Hero() {
   // const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
 
-  return <div>xx</div>;
+  return (
+    <div className="main-container">
+      <div className="title-container">
+        <h1 className="handwritten">Wine About It</h1>
+        <p className="handwritten">by Maya & QtCinderella</p>
+      </div>
+    </div>
+  );
 }
