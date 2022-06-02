611ab3272da3a7d234530c1eeb2f53c94e66f5c4
Artur Kazuś <tksd97@wp.pl>
2022-06-02 15:41:23 +0200

    hero bottles

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,17 +2,45 @@ import React from "react";
 // import { motion } from "framer-motion";
 
 import "./Styles.scss";
+import WineBottle from "../WineBottle/WineBottle";
 
 export default function Hero() {
   // const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
+  const color = "#322D2F";
 
   return (
     <div className="main-container">
       <div className="top-half-container">
-        <div className="title-container">
-          <h1 className="handwritten">Wine About It</h1>
-          <p className="handwritten">by Maya & QtCinderella</p>
+        <div className="vertical-66">
+          <div className="bottles-container">
+            <WineBottle
+              classes="bottle"
+              id="bottle1"
+              width="141px"
+              height="141px"
+              color={color}
+            />
+            <WineBottle
+              classes="bottle"
+              id="bottle2"
+              width="141px"
+              height="141px"
+              color={color}
+            />
+            <WineBottle
+              classes="bottle"
+              id="bottle3"
+              width="242px"
+              height="141px"
+              color={color}
+            />
+          </div>
+          <div className="title-container">
+            <h1 className="handwritten">Wine About It</h1>
+            <p className="handwritten">by Maya & QtCinderella</p>
+          </div>
         </div>
+        <div className="vertical-33"></div>
       </div>
     </div>
   );
