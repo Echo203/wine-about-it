b8a5261820dcc61f3c5ed092e89b6e1172b8c607
Artur Kazuś <tksd97@wp.pl>
2022-06-04 14:10:08 +0200

    Timeline component setup

---
+++ src/Components/Timeline/Timeline.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./Styles.scss";
+
+export default function Timeline() {
+  return (
+    <div className="timeline-section">
+      <div className="timeline-section-container">
+        <div className="timeline-section-title-container">
+          <h2 className="timeline-section-title handwritten">
+            Episode History
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Layouts/Home.jsx
+++ src/Layouts/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../Components/Hero/Hero";
 import ClipsAccordion from "../Components/ClipsAccordion/ClipsAccordion";
+import Timeline from "../Components/Timeline/Timeline";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <ClipsAccordion />
+      <Timeline />
     </>
   );
 }
