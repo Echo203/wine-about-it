bcdd9e4c9d890826d6ea862983eead7e59401425
Artur Kazuś <tksd97@wp.pl>
2022-06-04 16:18:48 +0200

    Timeline section top container added

--- src/Components/Timeline/Timeline.jsx
+++ src/Components/Timeline/Timeline.jsx
@@ -2,14 +2,19 @@ import React from "react";
 
 import "./Styles.scss";
 
+import WineServing from "../SVG components/WineServing";
+
 export default function Timeline() {
   return (
     <div className="timeline-section">
       <div className="timeline-section-container">
-        <div className="timeline-section-title-container">
-          <h2 className="timeline-section-title handwritten">
-            Episode History
-          </h2>
+        <div className="timeline-section-top-container">
+          <div className="timeline-section-title-container">
+            <h2 className="timeline-section-title handwritten">
+              Episode History
+            </h2>
+          </div>
+          <WineServing />
         </div>
       </div>
     </div>
