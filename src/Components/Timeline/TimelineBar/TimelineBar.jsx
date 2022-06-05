e7ba5094095364ffa344503b19f05d14eb0a50ed
Artur Kazuś <tksd97@wp.pl>
2022-06-05 16:32:08 +0200

    timeline bar animation

--- src/Components/Timeline/TimelineBar/TimelineBar.jsx
+++ src/Components/Timeline/TimelineBar/TimelineBar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 import "./Styles.scss";
 
@@ -7,7 +8,18 @@ import TimelineDot from "../../SVG components/TimelineDot";
 export default function TimelineBar() {
   return (
     <div className="timelinebar-container">
-      <div className="redbar"></div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ amount: 1 }}
+        className="redbar-top"
+      ></motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ amount: 1 }}
+        className="redbar-bot"
+      ></motion.div>
       <div className="glassbar"></div>
       <TimelineDot />
     </div>
